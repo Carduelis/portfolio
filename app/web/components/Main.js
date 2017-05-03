b69d4471bdaa2ac2a4af720a9b14ef60195e4b18
Carduelis <pavepy@gmail.com>
2017-05-03 17:37:49 +0300

    Top page

--- app/web/components/Main.js
+++ app/web/components/Main.js
@@ -1,16 +1,100 @@
 import React, { Component } from 'react';
+import MdAllInclusive from 'react-icons/lib/md/all-inclusive';
+import MdBusinessCenter from 'react-icons/lib/md/business-center';
+import MdSchool from 'react-icons/lib/md/school';
+import MdDirectionsBike from 'react-icons/lib/md/directions-bike';
+import MdLocalRestaurant from 'react-icons/lib/md/local-restaurant';
+import MdMusicNote from 'react-icons/lib/md/music-note';
+// import MdFavoriteOutline from 'react-icons/lib/md/favorite-outline';
+import MdInsertEmoticon from 'react-icons/lib/md/insert-emoticon';
+import MdDevicesOther from 'react-icons/lib/md/devices-other';
+import MdFavorite from 'react-icons/lib/md/favorite';
+import MdFreeBreakfast from 'react-icons/lib/md/free-breakfast';
+
 import ProjectsList from '../containers/ProjectsList';
 import AboutMe from '../containers/AboutMe';
 
+
 class Main extends Component {
   render() {
+    const separator = (
+      <div className="separator">
+        <div className="separator-line">
+          <MdAllInclusive />
+        </div>
+      </div>
+    );
+    const Header = ({ title, children, icon }) => (
+      <header className="centered">
+        <h2>
+          {icon && <span className="icon-lg">{icon}</span>}
+          {icon && <br />}
+          {title}
+        </h2>
+        {children}
+      </header>
+    );
     return (
       <div>
         <div className="content">
-          <h2>About me</h2>
           <AboutMe />
-          <h2>Projects</h2>
         </div>
+        <div className="overflow-scrolling columns-custom-wrapper">
+        <div className="columns columns-custom">
+          <div className="column skew">
+            <Header title="Education" icon={<MdSchool />} />
+            <p>I am a&nbsp;<b><abbr title="Upper bachelor degree">specialist</abbr> in Computer science</b>,
+              graduated from&nbsp;
+              <a
+                className="silent"
+                href="//english.mirea.ru"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+              Moscow Technological University
+            </a>
+            </p>
+            <p className="silent small">During the study period (Sep&nbsp;2010 – Jul&nbsp;2015) I
+ had excellent marks <nobr>4.98 out of 5</nobr>. Also I have published several scientific papers and a student book for internal use.
+I had participate in organization of conferences in the University.</p>
+          </div>
+          <div className="column skew">
+            <Header title="Work Experience" icon={<MdBusinessCenter />} />
+            <p>I started in the distant 2010,
+              so my experience counts <b className="accent">more than&nbsp;
+              {new Date().getFullYear() - 2010 - 1} years</b>.
+              It was a freelance time.
+            </p>
+            <p>From 2013 to 2015 I have developed more than&nbsp;
+              <b className="accent">40&nbsp;mobile&nbsp;websites</b>.
+              <br />Thanks to <a
+                className="silent"
+                href="//eski.mobi/worx"
+                rel="noopener noreferrer"
+                target="_blank"
+              >eski.mobi</a>!
+            </p>
+            <p>Since 2015 I work in MTU develop different Information-analytical
+              systems (GIS, FAIP, Universities Data Bank)
+              for <b className="accent">Ministry of Education and Science</b> of Russia
+            </p>
+          </div>
+          <div className="column skew">
+            <Header title="Make me happy" icon={<MdFavorite />} />
+            <p>Exploring new business ideas and opportunities</p>
+            <ul>
+              <li>Design and Front-end <MdInsertEmoticon /></li>
+              <li>Espresso <MdFreeBreakfast /></li>
+              <li>Cycling <MdDirectionsBike /></li>
+              <li>Music <MdMusicNote /></li>
+              <li>Bacon and fried eggs <MdLocalRestaurant /></li>
+              <li>Gadgets and DIY <MdDevicesOther /></li>
+            </ul>
+          </div>
+        </div>
+        </div>
+        {separator}
+        <Header title="The projects I've created" />
         <div className="content">
           <ProjectsList />
         </div>
--- app/web/containers/AboutMe.js
+++ app/web/containers/AboutMe.js
@@ -4,11 +4,11 @@ import { connect } from 'react-redux';
 class AboutMe extends Component {
   render() {
     const daysInMonth = (month, year) => new Date(year, month, 0).getDate();
-    const currentMonthDaysCount = daysInMonth(new Date().getFullYear(), new Date().getMonth())
+    const currentMonthDaysCount = daysInMonth(new Date().getFullYear(), new Date().getMonth());
     const dateOfBirth = new Date(1992, 7, 31, 7).getTime();
     const now = new Date().getTime();
-    const deltaInDays = (now - dateOfBirth)/(1000*60*60*24);
-    const months = (deltaInDays / 365.083 - Math.floor(deltaInDays / 365.083)) * 12;
+    const deltaInDays = (now - dateOfBirth) / (1000 * 60 * 60 * 24);
+    const months = ((deltaInDays / 365.083) - Math.floor(deltaInDays / 365.083)) * 12;
     const days = (months - Math.floor(months)) * currentMonthDaysCount;
     const age = {
       years: Math.floor(deltaInDays / 365.083),
@@ -17,19 +17,21 @@ class AboutMe extends Component {
     };
     return (
       <div className="about-me">
-
-        <div className="avatar">
-          <img alt="me" src="https://pp.userapi.com/c836325/v836325135/37642/0288gCa4VI0.jpg" />
-        </div>
-        <div className="text">
+        <header className="centered">
+          <h1>Hey, I{"'"}m Pavel</h1>
+          <h3>Front-end developer and UI&nbsp;designer</h3>
+          <div className="avatar">
+            <img alt="me" src="https://pp.userapi.com/c836325/v836325135/37642/0288gCa4VI0.jpg" />
+          </div>
+          <div className="text">
           <p className="full-name">Full name:&nbsp;
-            <b>Pavel</b> Shchegolev
+          <b>Pavel</b> Shchegolev
           </p>
           <p className="age">Age:&nbsp;
-            <b>{age.years} years</b>, {age.months} months and {age.days} days
+          <b>{age.years} years</b>, {age.months} months and {age.days} days
           </p>
-          <p className=""></p>
-        </div>
+          </div>
+        </header>
       </div>
     );
   }
